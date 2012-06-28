9c919045aa1dd4a2442dd24876573009dc22c517
James C <james.jn.chiang@gmail.com>
2012-06-28 01:26:35 +0000

    enter button submits

--- chat.js
+++ chat.js
@@ -1,6 +1,9 @@
 var app = require('express').createServer();
 var io  = require('socket.io').listen(app);
 
+var redis = require('redis');
+var redis_client = redis.createClient();
+
 app.listen(8080);
 
 app.get('/', function(req, res) {
@@ -12,8 +15,21 @@ app.get('/libraries/jquery.min.js', function(req, res) {
 });
 
 io.sockets.on('connection', function(client) {
+  var cname;
+  client.on('join', function(name) {
+    cname = name;
+    redis_client.lrange('messages', 0, -1, function(err, messages){
+      messages = messages.reverse();
+      messages.forEach(function(message) {
+        send_msg = JSON.parse(message) 
+        client.emit("message", send_msg.name + ': ' + send_msg.data);
+      });
+    });    
+  }); 
   client.on('message', function(data) {
-    console.log(data);  
+    redis_client.lpush("messages", JSON.stringify({name: cname, data: data.message}), function(err, response) {
+      redis_client.ltrim("messages", 0, 10);
+    });
   })
 })
 
